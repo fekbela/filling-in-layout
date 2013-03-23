03823cb8bbf4946b04b1ce67efc2df9cb7a4e8e4
Bela F <feketekuty@yahoo.com>
2013-03-23 14:38:17 -0400

    filling-in-layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '243b3238816a22a118690483b2bc48738de4d5e2d79f0b04fb6a79109dca434c297324a363eb0f877cbf95294140fb971b0eb7b13fadcbde6f5835e9bcc272b0'
